7c0cecbb6c6c202bc3e690a455372052b18cea28
jsik <survivor2199@yahoo.com>
2017-10-25 00:13:54 -0700

    created github for flustudy

---
+++ FluStudy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FluStudy
+//
+//  Created by Jean Sik on 2017/10/25.
+//  Copyright © 2017 Jean Sik. All rights reserved.
+//
+
+import UIKit
+import ResearchKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
